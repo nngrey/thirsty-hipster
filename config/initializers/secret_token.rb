cebd40490c200cc7a925acc1929a230b79ff3af1
Nathan Grey & Jeremy Clemans <student@epicodus.com>
2014-04-24 09:32:15 -0700

    initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThirstyHipster::Application.config.secret_key_base = 'a1847db505b469884aeb312df09a6b466c49c5866c8795a25e54eafac4a3a41ad94b16a4150320efb1b282bb5bd99a522545c039f86cbed7084652b58d5aec80'
