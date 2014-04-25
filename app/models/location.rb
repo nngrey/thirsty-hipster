1ae4fe743c79821b164a0dc90cf6454f35ed3daa
Jeremy Clemans & Angela Yen <student@epicodus.com>
2014-04-25 10:46:43 -0700

    make queries on location against Yelp API before a new location is created
     Changes to be committed: authors Jeremy Clemans and Angela Yen

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,17 +6,36 @@ class Location < ActiveRecord::Base
 
   before_create :make_search
 
-
   private
 
   def make_search
 
-    response = RestClient::Request.new(
-      :method => :get,
-      :url => "http://api.yelp.com/v2/search?term=food&location=San+Francisco"
-      )
+    consumer_key = ENV['CONSUMER_KEY']
+    consumer_secret = ENV['CONSUMER_SECRET']
+    token = ENV['TOKEN']
+    token_secret = ENV['TOKEN_SECRET']
+    api_host = 'api.yelp.com'
+
+    consumer = OAuth::Consumer.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], {:site => "http://#{api_host}"})
+    access_token = OAuth::AccessToken.new(consumer, ENV['TOKEN'], ENV['TOKEN_SECRET'])
+
+    binding.pry
+
+    path = "/v2/search?category_filter=bars&location=Portland"
+    yelp_data = JSON.parse(access_token.get(path).body)
+
+    # ########################
+
+    # response = RestClient::Request.new(
+    #   :method => :get,
+    #   :url => "http://api.yelp.com/v2/search?category_filter=bars&location=#{@location.name}",
+    #   consumer = OAuth::Consumer.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], {:site => "http://#{api_host}"})
+    #   access_token = OAuth::AccessToken.new(consumer, ENV['TOKEN'], ENV['TOKEN_SECRET'])
+    #   ).execute
+
+    # api_host = 'api.yelp.com'
+
+    # yelp_data = JSON(access_token.get(response).body)
   end
 
 end
-
-
