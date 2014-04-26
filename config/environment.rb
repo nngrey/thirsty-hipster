7d1ac17b7bc77bfdda48f411b94a58b96b0e526e
Nathan Grey <greynng@gmail.com>
2014-04-26 15:50:46 -0700

    update time format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 ThirstyHipster::Application.initialize!
+
+Time::DATE_FORMATS[:quick] = "%l:%M %p"
---
+++ config/initializers/my_time_formats.rb
@@ -0,0 +1,3 @@
+# ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!({
+#   :quick => "%I:%M%p"
+# })
