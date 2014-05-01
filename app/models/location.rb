9a3e87197dfca47463a3f1d85f209b3786dfccd1
Nathan Grey & Angela Yen <student@epicodus.com>
2014-05-01 10:17:50 -0700

    refactor location.rb and search.rb models to remove extraneous code

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,34 +4,4 @@ class Location < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def yelp_check
-    # name = name.gsub(/\s/, '+')
-
-    # consumer_key = ENV['CONSUMER_KEY']
-    # consumer_secret = ENV['CONSUMER_SECRET']
-    # token = ENV['TOKEN']
-    # token_secret = ENV['TOKEN_SECRET']
-    api_host = 'api.yelp.com'
-
-    consumer = OAuth::Consumer.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], {:site => "http://#{api_host}"})
-    access_token = OAuth::AccessToken.new(consumer, ENV['TOKEN'], ENV['TOKEN_SECRET'])
-
-    path = "/v2/search?term=category_filter=bars&location=Portland&limit=10"
-
-    parsed_data = JSON.parse(access_token.get(path).body)
-
-    locations_data = parsed_data['businesses']
-
-    locations = []
-    locs = []
-
-    locations_data.each do |location|
-      new_location = Location.new(name: location["name"], address: location["location"]["address"][0], city: location["location"]["city"], state: location["location"]["state_code"], rating: location["rating_img_url"])
-      locs << new_location
-    end
-
-
-    locs
-  end
-
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -3,17 +3,12 @@ class Search
   def yelp_check(zip)
 
     api_host = 'api.yelp.com'
-    #city = @search.city
-
-    # address = @search.address
 
     consumer = OAuth::Consumer.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], {:site => "http://#{api_host}"})
     access_token = OAuth::AccessToken.new(consumer, ENV['TOKEN'], ENV['TOKEN_SECRET'])
 
     path = "/v2/search?term=bars&location=" + zip
 
-
-    # "/v2/search?term=category_filter=bars&location="+city+"&limit=20"
     parsed_data = JSON.parse(access_token.get(path).body)
 
     locations_data = parsed_data['businesses']
