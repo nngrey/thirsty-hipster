ef47d055c3b81a7fb6fa3fcf868c19945dea9cf7
Nathan Grey <greynng@gmail.com>
2014-04-30 20:36:17 -0700

    small edit to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,26 +7,22 @@ class Location < ActiveRecord::Base
   def yelp_check
     # name = name.gsub(/\s/, '+')
 
-    consumer_key = ENV['CONSUMER_KEY']
-    consumer_secret = ENV['CONSUMER_SECRET']
-    token = ENV['TOKEN']
-    token_secret = ENV['TOKEN_SECRET']
+    # consumer_key = ENV['CONSUMER_KEY']
+    # consumer_secret = ENV['CONSUMER_SECRET']
+    # token = ENV['TOKEN']
+    # token_secret = ENV['TOKEN_SECRET']
     api_host = 'api.yelp.com'
 
     consumer = OAuth::Consumer.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], {:site => "http://#{api_host}"})
     access_token = OAuth::AccessToken.new(consumer, ENV['TOKEN'], ENV['TOKEN_SECRET'])
 
-    path = "/v2/search?term=#{name}&category_filter=bars&location=Portland&limit=5"
+    path = "/v2/search?term=category_filter=bars&location=Portland&limit=5"
     parsed_data = JSON.parse(access_token.get(path).body)
 
     locations_data = parsed_data['businesses']
 
     locations = []
     locs = []
-
-    # locations_data.each do |location|
-    #   locations << location.reject {|key, value| !["name", "location", "rating_img_url"].include?(key)}
-    # end
 
     locations_data.each do |location|
       new_location = Location.new(name: location["name"], address: location["location"]["address"][0], city: location["location"]["city"], state: location["location"]["state_code"], rating: location["rating_img_url"])
